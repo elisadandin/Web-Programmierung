43c1959fcaa0059a4c693d5b29234257728b6b28
elisadandin <elisa.dandin@web.de>
2021-01-25 15:18:42 +0100

    Header css angepasst

--- onlineshop/src/components/Header.js
+++ onlineshop/src/components/Header.js
@@ -3,6 +3,7 @@ import Menu from './svg/bars-solid.svg'
 import Close from './svg/times-solid.svg'
 import CartIcon from './svg/shopping-cart-solid.svg'
 import {Link} from 'react-router-dom'
+import './css/Header.css'
 
 export class Header extends Component {
     render() {
