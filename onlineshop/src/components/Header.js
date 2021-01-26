96a4120dcfae6891aa2e6e44b48cd3d4073aec1b
elisadandin <elisa.dandin@web.de>
2021-01-26 11:19:29 +0100

    Toggle Mechanismus verwendet Toggle-Menü wird auf einem schmalen Viewport angezeigt

--- onlineshop/src/components/Header.js
+++ onlineshop/src/components/Header.js
@@ -5,20 +5,32 @@ import CartIcon from './svg/shopping-cart-solid.svg'
 import {Link} from 'react-router-dom'
 import './css/Header.css'
 
+
 export class Header extends Component {
+
+    state = {
+        toggle: false
+    }
+
+    menuToggle = () =>{
+        this.setState({toggle: !this.state.toggle})
+    }
+
+
     render() {
+        const {toggle} = this.state;
         return (
             <header>
-                <div className="menu">
+                <div className="menu" onClick={this.menuToggle}>
                     <img src={Menu} alt="" width="20"/>
                 </div>
                 <div className="logo">
                     <h1><Link to ="/">MyFitnessplace</Link></h1>
                 </div>
                 <nav>
-                    <ul>
-                        <li><Link to="/">Shop</Link></li>
-                        <li className='close'>
+                    <ul className={toggle ? "toggle": ""}>
+                        <li><Link to="/shop">Shop</Link></li>
+                        <li className="close" onClick={this.menuToggle}>
                             <img src={Close} alt="" width="20"/>
                         </li>
                     </ul>
