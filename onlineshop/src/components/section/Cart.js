ff81ae3fede30fdb04802089cf6ce24658fd9c46
elisadandin <elisa.dandin@web.de>
2021-02-01 13:48:12 +0100

    Produkte können zum Warenkorb hinzugefügt werden

--- onlineshop/src/components/section/Cart.js
+++ onlineshop/src/components/section/Cart.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import {DataContext} from '../Context'
+import '../css/Cart.css'
 
 export class Cart extends Component {
+    static contextType = DataContext;
+
     render() {
+        const {cart} = this.context;
         return (
-            <div>
-                Cart Component
-            </div>
+            <>
+                {
+                    cart.map(item =>(
+                        <div className="products" key={item._id}>
+                            <img src={item.src} alt=""/>
+                            <div className="box">
+                                <div className="row">
+                                    <h2>{item.title}</h2>
+                                    <span>${item.price}</span>
+                                </div>
+                                <p>{item.description}</p>
+                                <p>{item.content}</p>
+                                <Link to="cart" className="cart">
+                                    Add to Cart
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                }
+            </>
+                    
+           
         )
     }
 }
