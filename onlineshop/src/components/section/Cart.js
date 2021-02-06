38f86c1c5710621800ebb74c02163fd1037e6c84
elisadandin <elisa.dandin@web.de>
2021-02-06 13:02:06 +0100

    If - else Schleife im Warenkorb benutzt

--- onlineshop/src/components/section/Cart.js
+++ onlineshop/src/components/section/Cart.js
@@ -8,37 +8,41 @@ export class Cart extends Component {
 
     render() {
         const {cart, increase, reduction, remove} = this.context;
-        return (
-            <>
-                {
-                    cart.map(item =>(
-                        <div className="CartItem" key={item._id}>
-                            <img src={item.src} alt=""/>
-                            <div className="box">
-                                <div className="row">
-                                    <h2>{item.title}</h2>
-                                    <span>${item.price}</span>
+        if(cart.lenght === 0){
+            return <h2 style={{textAlign:"center"}}>Warenkorb ist leer</h2>
+        }else{
+            return (
+                <>
+                    {
+                        cart.map(item =>(
+                            <div className="CartItem" key={item._id}>
+                                <img src={item.src} alt=""/>
+                                <div className="box">
+                                    <div className="row">
+                                        <h2>{item.title}</h2>
+                                        <span>${item.price}</span>
+                                    </div>
+                                    <p>{item.description}</p>
+                                    <p>{item.content}</p>
+                                    <div className="amount">
+                                        <button className="count" onClick={() => reduction(item._id)}> - </button>
+                                        <span>{item.count}</span>
+                                        <button className="count" onClick={() => increase(item._id)}> + </button>
+                                    </div>
                                 </div>
-                                <p>{item.description}</p>
-                                <p>{item.content}</p>
-                                <div className="amount">
-                                    <button className="count" onClick={() => reduction(item._id)}> - </button>
-                                    <span>{item.count}</span>
-                                    <button className="count" onClick={() => increase(item._id)}> + </button>
-                                </div>
+                                <div className="delete" onClick={() => remove(item._id)}>X</div>
                             </div>
-                            <div className="delete" onClick={() => remove(item._id)}>X</div>
-                        </div>
-                    ))
-                }
-                <div className="total">
-                    <Link to="/payment"> Bezahlen </Link>
-                    <h3>Gesamtsumme: 0</h3>
-
-                </div>
-            </>
-        )
-    }
+                        ))
+                    }
+                    <div className="total">
+                        <Link to="/payment"> Bezahlen </Link>
+                        <h3>Gesamtsumme: 0</h3>
+    
+                    </div>
+                </>
+                )
+            }
+        }    
 }
 
 export default Cart
