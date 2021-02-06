872b43a24bbb42a620029e1055177c26e70ebe7c
elisadandin <elisa.dandin@web.de>
2021-02-06 14:42:34 +0100

    Installation Matrial-Ui - Importierung von Material-Ui Modulen

--- onlineshop/src/components/payment/FormUserDetails.js
+++ onlineshop/src/components/payment/FormUserDetails.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+
+
 
 export class FormUserDetails extends Component {
     render() {
