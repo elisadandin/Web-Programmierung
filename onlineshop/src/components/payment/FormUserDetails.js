e483616e8931f03678591da3f3225ddf0036b650
elisadandin <elisa.dandin@web.de>
2021-02-06 15:26:46 +0100

    Kontaktformular erstellt - Modul AppBar: Titelleiste - Modul TextFiel: Eingabefelder

--- onlineshop/src/components/payment/FormUserDetails.js
+++ onlineshop/src/components/payment/FormUserDetails.js
@@ -2,15 +2,48 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton'
 
 
 
 export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
     render() {
+        const { values, handleChange } = this.props;
         return (
-            <div>
-                <h1>Hello from User Details</h1>
-            </div>
+            <MuiThemeProvider>
+              <React.Fragment>
+                  <AppBar title="Kontaktdaten" />
+                  <TextField
+                  floatingLabelText="Vorname"
+                  onChange={handleChange('firstName')}
+                  defaultValue={values.firstName}
+                  />
+                  <br/>
+                  <TextField
+                  floatingLabelText="Nachname"
+                  onChange={handleChange('lastName')}
+                  defaultValue={values.lastName}
+                  />
+                  <br/>
+                  <TextField
+                  floatingLabelText="Adresse"
+                  onChange={handleChange('address')}
+                  defaultValue={values.address}
+                  />
+                  <br/>
+                  <TextField
+                  floatingLabelText="E-Mail"
+                  onChange={handleChange('email')}
+                  defaultValue={values.email}
+                  />
+
+              </React.Fragment>
+            </MuiThemeProvider>
         )
     }
 }
